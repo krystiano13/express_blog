fd6cfcf264914b1a8765fc3342c0acf17e537fe8
krystiano13 <crystianotv@gmail.com>
2024-06-03 18:30:23 +0200

    add passport js and session middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import "./auth/local_strategy";
+import MongoStore from "connect-mongo";
 
 export function createApp() {
   const app = express();
 
   app.use(express.json());
   app.use(cookieParser());
+  app.use(
+    session({
+      secret: process.env.SECRET || "keyboard cat",
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge: 3600000 },
+      store: MongoStore.create({
+        //@ts-ignore
+        client: mongoose.connection.getClient(),
+      }),
+    })
+  );
 
   app.use(
     cors({
@@ -15,6 +32,10 @@ export function createApp() {
       credentials: true,
     })
   );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(passport.authenticate("session"));
 
   return app;
 }
