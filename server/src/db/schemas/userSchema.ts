0523529c455267dd6fe302ed7dc5e82f4cc7fb22
krystiano13 <crystianotv@gmail.com>
2024-06-03 17:49:35 +0200

    create schema for user

---
+++ server/src/db/schemas/userSchema.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  email: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  password: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  role: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    default: "user",
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
