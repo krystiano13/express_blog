56d42b301d872d2e87e3ef093028f93214b653f0
krystiano13 <crystianotv@gmail.com>
2024-06-04 10:23:38 +0200

    create middleware for checking if user is logged in or not

---
+++ server/src/middleware/user.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import type { User } from "../types/auth";
+import type { Session } from "express-session";
+
+export function notLoggedIn(req: Request, res: Response, next: NextFunction) {
+  const { session }: { session: Session & Partial<{ passport: User }> } = req;
+
+  if (!session.passport) {
+    return res.status(403).send({ error: "Unauthorized" });
+  } else {
+    next();
+  }
+}
+
+export function loggedIn(req: Request, res: Response, next: NextFunction) {
+  const { session }: { session: Session & Partial<{ passport: User }> } = req;
+
+  if (session.passport) {
+    return res.status(401).send({ error: "You can't be logged in to do that" });
+  } else {
+    next();
+  }
+}
