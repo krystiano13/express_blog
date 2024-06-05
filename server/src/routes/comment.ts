292c7686c7923d0829f7de8e0ceb44abd4d4f661
krystiano13 <crystianotv@gmail.com>
2024-06-05 12:36:10 +0200

    create function for getting comments from database

--- server/src/routes/comment.ts
+++ server/src/routes/comment.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { notLoggedIn } from "../middleware/user";
+import { getComments } from "./endpoints/comment_endpoints";
 
 const router = Router();
 
-router.get("/api/comments/:post_id");
+router.get("/api/comments/:post_id", notLoggedIn, getComments);
 router.post("/api/comments/:post_id");
 router.patch("/api/comments/:post_id/:id");
 router.delete("/api/comments/:post_id/:id");
